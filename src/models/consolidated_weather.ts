13f791eee8a5147efcffd4eb532283ffdcbcdb06
Yomar Miranda <mirandayomar@gmail.com>
2022-03-04 19:53:30 -0500

    feat: create some location and consolidated weather models (interfaces).

---
+++ src/models/consolidated_weather.ts
@@ -0,0 +1,19 @@
+interface ConsolidatedWeather {
+  id: number;
+  weather_state_name: string;
+  weather_state_abbr: string;
+  wind_direction_compass: string;
+  created: string;
+  applicable_date: string;
+  min_temp: number;
+  max_temp: number;
+  the_temp: number;
+  wind_speed: number;
+  wind_direction: number;
+  air_pressure: number;
+  humidity: number;
+  visibility: number;
+  predictability: number;
+}
+
+export default ConsolidatedWeather;
---
+++ src/models/location.ts
@@ -0,0 +1,8 @@
+interface PlaceLocation {
+  latt_long: string;
+  location_type: string;
+  title: string;
+  woeid: string; // Where On Earth ID
+}
+
+export default PlaceLocation;
