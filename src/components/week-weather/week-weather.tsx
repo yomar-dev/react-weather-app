6f18d4de60f2d8877917a28e8906616e0af79666
Yomar Miranda <mirandayomar@gmail.com>
2022-03-04 20:00:01 -0500

    feat: create week weather component.

---
+++ src/components/week-weather/week-weather.tsx
@@ -0,0 +1,35 @@
+import WeatherCard from './weather-card/weather-card';
+
+import styles from './week-weather.module.scss';
+import ConsolidatedWeather from '../../models/consolidated_weather';
+
+interface Props {
+  weekWeather: ConsolidatedWeather[];
+}
+
+function WeekWeather({ weekWeather }: Props) {
+  return (
+    <section className={styles.container}>
+      {weekWeather &&
+        weekWeather.map((weather: ConsolidatedWeather) => {
+          let { id, max_temp, min_temp, applicable_date } = weather;
+          let formattedDate = new Intl.DateTimeFormat('en', {
+            weekday: 'short',
+            day: '2-digit',
+            month: 'short',
+            timeZone: 'America/Bogota',
+          }).format(new Date(applicable_date));
+          return (
+            <WeatherCard
+              date={formattedDate}
+              tempMax={Math.round(max_temp)}
+              tempMin={Math.round(min_temp)}
+              key={id}
+            />
+          );
+        })}
+    </section>
+  );
+}
+
+export default WeekWeather;
