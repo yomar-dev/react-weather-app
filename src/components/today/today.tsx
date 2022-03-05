497e72e97624cbc2dec1e0cbfebefda3cb3b1c24
Yomar Miranda <mirandayomar@gmail.com>
2022-03-04 20:58:35 -0500

    feat: display today's weather with real data.

--- src/components/today/today.tsx
+++ src/components/today/today.tsx
@@ -1,7 +1,22 @@
 import styles from './today.module.scss';
 import showerImg from '../../assets/images/shower.png';
+import ConsolidatedWeather from '../../models/consolidated_weather';
 
-function Today() {
+interface Props {
+  city: string;
+  weather: ConsolidatedWeather;
+}
+
+function Today({ city, weather }: Props) {
+  const { weather_state_name, the_temp, applicable_date } = weather;
+
+  let formattedDate = new Intl.DateTimeFormat('en', {
+    weekday: 'short',
+    day: '2-digit',
+    month: 'short',
+    timeZone: 'America/Bogota',
+  }).format(new Date(applicable_date));
+
   return (
     <div className={styles.sidebar}>
       <header className={styles['sidebar-header']}>
@@ -20,18 +35,22 @@ function Today() {
           />
         </figure>
         <h2 className={styles['sidebar-main__temperature']}>
-          15
+          {the_temp}
           <span className={styles['sidebar-main__temperature-unit']}>
             &#8451;
           </span>
         </h2>
-        <p className={styles['sidebar-main__weather-state']}>Shower</p>
+        <p className={styles['sidebar-main__weather-state']}>
+          {weather_state_name}
+        </p>
       </section>
 
       <footer className={styles['sidebar-footer']}>
-        <p className={styles['sidebar-footer__date']}>Today • Fri, 5 Jun</p>
+        <p className={styles['sidebar-footer__date']}>
+          Today • {formattedDate}
+        </p>
         <p className={styles['sidebar-footer__place']}>
-          <span className="material-icons">place</span> Helsinki
+          <span className="material-icons">place</span> {city}
         </p>
       </footer>
     </div>
